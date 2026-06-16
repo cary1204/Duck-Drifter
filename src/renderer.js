6ce90f4919a57c400604774fee34ebe031459a95
Cary <homework.cary@gmail.com>
2026-06-16 19:36:42 -0400

    revert changes due to dizy bg

--- src/renderer.js
+++ src/renderer.js
@@ -8,19 +8,48 @@ const renderer = {
         this.bgImg.src = 'assets/BG.png';
     },
 
+
     drawBackground(ctx, cameraX, cameraY) {
         const W = ctx.canvas.width;
         const H = ctx.canvas.height;
 
-        const offX = ((-cameraX % this.bgImg.width) + this.bgImg.width) % this.bgImg.width;
-        const offY = ((-cameraY % this.bgImg.height) + this.bgImg.height) % this.bgImg.height;
+      // water
+        ctx.fillStyle = '#0496c7';
+        ctx.fillRect(0, 0, W, H);
 
-        for (let x = offX - this.bgImg.width; x < W + this.bgImg.width; x += this.bgImg.width) {
-            for (let y = offY - this.bgImg.height; y < H + this.bgImg.height; y += this.bgImg.height) {
-                ctx.drawImage(this.bgImg, x, y);
-            }
+      // grid
+        const gridSize = 128;
+        const offX = ((-cameraX % gridSize) + gridSize) % gridSize;
+        const offY = ((-cameraY % gridSize) + gridSize) % gridSize;
+
+        ctx.strokeStyle = '#ffffff';
+        ctx.lineWidth = 1;
+        for (let x = offX - gridSize; x < W + gridSize; x += gridSize) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, H);
+        ctx.stroke();
+        }
+        for (let y = offY - gridSize; y < H + gridSize; y += gridSize) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(W, y);
+        ctx.stroke();
         }
     },
+    // drawBackground(ctx, cameraX, cameraY) {
+    //     const W = ctx.canvas.width;
+    //     const H = ctx.canvas.height;
+
+    //     const offX = ((-cameraX % this.bgImg.width) + this.bgImg.width) % this.bgImg.width;
+    //     const offY = ((-cameraY % this.bgImg.height) + this.bgImg.height) % this.bgImg.height;
+
+    //     for (let x = offX - this.bgImg.width; x < W + this.bgImg.width; x += this.bgImg.width) {
+    //         for (let y = offY - this.bgImg.height; y < H + this.bgImg.height; y += this.bgImg.height) {
+    //             ctx.drawImage(this.bgImg, x, y);
+    //         }
+    //     }
+    // },
 
     drawDuck(ctx) {
         const W = ctx.canvas.width;
